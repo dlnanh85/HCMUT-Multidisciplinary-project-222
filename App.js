6db4a27e114ec1a478c0e06da60c5e822884bd5f
Anh Dinh <dinhlenhatanh2002yahoo@gmail.com>
2023-04-12 09:50:25 +0700

    small fix

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import SignIn from "./screens/SignIn";
 import DoorControl from "./screens/DoorControl";
-import Home2 from "./screens/Home2";
-import LightingSystem2 from "./screens/LightingSystem2";
+import Home from "./screens/Home";
+import LightingSystem from "./screens/LightingSystem";
 import Camera from "./screens/Camera";
 import Camera1 from "./screens/Camera1";
 import LogInDoor from "./screens/LogInDoor";
@@ -47,13 +47,13 @@ const App = () => {
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name="Home2"
-              component={Home2}
+              name="Home"
+              component={Home}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name="LightingSystem2"
-              component={LightingSystem2}
+              name="LightingSystem"
+              component={LightingSystem}
               options={{ headerShown: false }}
             />
             <Stack.Screen
